85622c1b7e128e01f10cc6f4b0b9256b0d2775e5
Yenisbel Valle <yenisbelv@gmail.com>
2020-04-26 17:22:22 -0700

    Add module styling for header and layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import headerStyles from "./header.module.scss"
 
 const Header = () => {
   return (
-    <header>
-      <h1>Yenisbel Valle</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          Yenisbel Valle
+        </Link> 
+      </h1>
       <nav>
-        <ul>
-          <li><Link to="/contact"> Contact </Link></li>
-          <li><Link to="/about"> About </Link></li>
-          <li><Link to="/blog"> Blog </Link></li>
-          <li><Link to="/"> Home </Link></li>
+        <ul className={headerStyles.navList}>
+          <li><Link className={headerStyles.navLinks} activeClassName={headerStyles.activeNavLinks} to="/contact"> Contact </Link></li>
+          <li><Link className={headerStyles.navLinks} activeClassName={headerStyles.activeNavLinks} to="/about"> About </Link></li>
+          <li><Link className={headerStyles.navLinks} activeClassName={headerStyles.activeNavLinks} to="/blog"> Blog </Link></li>
+          <li><Link className={headerStyles.navLinks} activeClassName={headerStyles.activeNavLinks} to="/"> Home </Link></li>
         </ul>
       </nav>
     </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = (props) => {
   return (
-    <div>
-      <Header/>
-      <p>{props.children}</p>
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header/>
+        {props.children}
+      </div>
       <Footer/>
     </div>
   )
