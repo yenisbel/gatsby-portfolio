a32f7c5564a4ba823a8419d8ccbea27da24820d6
Yenisbel Valle <yenisbelv@gmail.com>
2020-04-28 16:09:50 -0700

    Add styling for project page and footer

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql, useStaticQuery, Link } from "gatsby"
+import projectStyles from "./project.module.scss"
 
 const ProjectPage = () => {
   const data = useStaticQuery(graphql`
@@ -24,15 +25,14 @@ const ProjectPage = () => {
   return (
     <Layout> 
       <h1>Projects</h1>
-      <ol>
+      <ol className={projectStyles.projects}>
         {data.allMarkdownRemark.edges.map(project => {
           return(
-            <li>
+            <li className={projectStyles.project}>
               <h2>
                 <Link to={`project/${project.node.fields.slug}`}>
                   {project.node.frontmatter.title}
-                </Link>
-                
+                </Link> 
               </h2>
               <p>Stack used: {project.node.frontmatter.stack}</p>
             </li>
