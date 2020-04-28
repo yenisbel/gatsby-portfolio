6a2b3dc8705756291feb670fcb2570308953773e
Yenisbel Valle <yenisbelv@gmail.com>
2020-04-27 18:29:06 -0700

    Add gatsby node file to generate slug for each project

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+const path = require('path')
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+  // Transform the new node here and create a new node or
+  // create a new node field.
+  if(node.internal.type === "MarkdownRemark"){
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    createNodeField({
+      value: slug,
+      name:"slug",
+      node
+    })
+  }
+  
+}
