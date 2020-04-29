7859597e28c1d94af2295590eb5f0cf23016bf5e
Yenisbel Valle <yenisbelv@gmail.com>
2020-04-28 22:10:32 -0700

    Add refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
     <Layout>
       <Head title="Home"/>
       <h1>Welcome to my portfolio</h1>
-      <p>My name is Yenisbel, a frontend developer living in the Bay Area</p>
+      <p>My name is Yenisbel Valle, a frontend developer living in the Bay Area</p>
       <p>Need a developer? <Link to="/contact">Contact me</Link></p>
     </Layout>
     
